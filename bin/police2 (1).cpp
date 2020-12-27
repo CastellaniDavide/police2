fb3cd1d0fe670913cdb449e6553064a93ee9542a
DavideC03 <davide.castellani.5.8.03@gmail.com>
2020-12-27 10:24:00 +0100

    2020-12-27

--- bin/police2 (1).cpp
+++ bin/police2 (1).cpp
@@ -4,10 +4,9 @@
  * NOTE: it is recommended to use this even if you don't
  * understand the following code.
  */
-
+#include <iostream>
 #include <assert.h>
 #include <stdio.h>
-#include <vector>
 #include <algorithm>
 
 using namespace std; 
@@ -21,7 +20,7 @@ int V[MAXN];
 
 int main() {
   //  uncomment the following lines if you want to read/write from files
-  // freopen("input1.txt", "r", stdin);
+  // freopen("input0.txt", "r", stdin);
   // freopen("output.txt", "w", stdout);
 
   assert(1 == scanf("%d", &N));
@@ -29,9 +28,31 @@ int main() {
     assert(1 == scanf("%d", &V[i]));
 
   int loop_max = 0;
-  
-  for(int i = 0; i < 3; i++){
+
+  for(int i = 0; i < N; i++){
+
+    int loop = 0;
     
+    int visited[MAXN] = {0};
+    int loop_iter[MAXN] = {0};
+    int loop_number = 0;
+    int house_to_visit = i;
+
+
+    while (!(visited[house_to_visit])){
+      visited[house_to_visit] = 1;
+      loop_iter[house_to_visit] = loop_number;
+      house_to_visit = V[house_to_visit];
+      loop_number++;
+    }
+
+    loop = loop_number - loop_iter[house_to_visit];
+
+    if (loop > loop_max)
+      loop_max = loop;
+
+
+    /*
     int loop = 1;
     vector<int> visited; 
     int house_to_visit = i;
@@ -45,16 +66,19 @@ int main() {
       loop = visited.size();
     }
     else{
-      int j = visited.size() - 1;
-      while ((visited[j] != house_to_visit)&(j>=0)){
-        j--;
-        loop++;
-      }
+      std::vector<int>::iterator itr = std::find(v.begin(), v.end(), house_to_visit);
+      std::distance(v.begin(), itr);
+
+      //int j = visited.size() - 1;
+      //while ((visited[j] != house_to_visit)&(j>=0)){
+      //  j--;
+      //  loop++;
+      //}
     }
 
     if (loop > loop_max)
       loop_max = loop;
-    
+    */
   }
 
   printf("%d\n", loop_max); // print the result
